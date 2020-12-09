f385e4a506dd34f74c0d48df0e3e035c94e0b036
Vladimir <girba.vladimir@yandex.ru>
2020-12-09 15:43:49 +0300

    module3-task1

--- src/view/main-menu.js
+++ src/view/main-menu.js
@@ -1,3 +1,5 @@
+import {createElement} from "../util";
+
 const getWatchListAmount = (films) => {
   return films.filter((el) => el.isNeedToWatch).length;
 };
@@ -10,7 +12,7 @@ const getFavoriteFilmsAmount = (films) => {
   return films.filter((el) => el.isFavorite).length;
 };
 
-export const createMainMenu = (films) => {
+const createMainMenu = (films) => {
   return `<nav class="main-navigation">
     <div class="main-navigation__items">
       <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
@@ -27,3 +29,27 @@ export const createMainMenu = (films) => {
     <li><a href="#" class="sort__button">Sort by rating</a></li>
   </ul>`;
 };
+
+export default class MainMenu {
+  constructor(filmsList) {
+    this._filmsList = filmsList;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createMainMenu(this._filmsList);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
